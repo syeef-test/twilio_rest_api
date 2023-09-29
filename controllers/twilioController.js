4c6e12c0b7618bcb2253671e031b41017de6b3c7
syeef <syeefislam@yahoo.com>
2023-09-29 13:06:55 +0530

    whatsapp message send succesful

--- controllers/twilioController.js
+++ controllers/twilioController.js
@@ -58,9 +58,43 @@ exports.call = async (req, res, next) => {
 
 exports.whatsapp = async (req, res, next) => {
   try {
-    console.log(req.body);
+    const reciepient_phone_no = req.body.to;
+
+    //First check number is string and not null
+    if (
+      typeof reciepient_phone_no !== "string" ||
+      reciepient_phone_no.trim() === ""
+    ) {
+      return res.status(400).json({ message: "Invalid Phone Number" });
+    }
+
+    //Check phone number is valid or not
+    const valid_phone_number_details = await checkPhoneNumber(
+      reciepient_phone_no
+    );
+
+    const valid = valid_phone_number_details.valid;
+    const valid_error = valid_phone_number_details.errors;
+
+    if (!valid && valid_error.length > 0) {
+      return res.status(400).json({ message: "Invalid Phone Number" });
+    } else {
+      //send whatsappmessage
+      const response = await client.messages.create({
+        body: "Your appointment is coming up on July 21 at 3PM",
+        from: `whatsapp:${process.env.TWILIO_WHATSAPP_PHONE_NUMBER}`,
+        to: `whatsapp:${reciepient_phone_no}`,
+      });
+      if (response.sid) {
+        console.log(response.sid);
+        return res
+          .status(200)
+          .json({ message: "Whatsapp message sent successfully" });
+      }
+    }
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "An error occured on server" });
   }
 };
 
